3f0e9bdf4fbd1c3efc48c633ce6449333fb68231
wuyao123 <yao.wu@aliyun.com>
2017-06-24 18:04:55 +0800

    platform
    Vue2.0+vue-resource+vue-router+lodash

---
+++ platform/src/components/quitForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login-form">
+    <div class="g-form-btn quit">
+      <p>确认退出吗？</p>
+      <a class="button" @click="okClick">确定</a>
+      <a class="button" @click="cancelClick">取消</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isShow:{
+      type:Boolean,
+      default:false
+    }
+  },
+  methods:{
+    okClick(){
+      this.$emit('ok-quit')
+    },
+    cancelClick(){
+      this.$emit('cancel-quit')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .quit{
+    padding-left:0;
+    text-align:center;
+  }
+  .quit p{
+    line-height: 24px;
+    font-size:16px;
+    margin-bottom: 10px;
+  }
+</style>
---
+++ platform/src/components/regForm.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="login-form">
+    <div class="g-form">
+      <div class="g-form-line">
+        <span class="g-form-label">用户名：</span>
+        <div class="g-form-input">
+          <input type="text" v-model="usernameModel" placeholder="请输入用户名">
+        </div>
+        <span class="g-form-error">{{ userErrors.errorText }}</span>
+      </div>
+      <div class="g-form-line">
+        <span class="g-form-label">密码：</span>
+        <div class="g-form-input">
+          <input type="password" v-model="passwordModel1" placeholder="请输入密码">
+        </div>
+        <span class="g-form-error">{{ passwordErrors.errorText }}</span>
+      </div>
+      <div class="g-form-line">
+        <span class="g-form-label">重复密码：</span>
+        <div class="g-form-input">
+          <input type="password" v-model="passwordModel2" placeholder="请输入密码">
+        </div>
+        <span class="g-form-error">{{ passwordSameErrors.errorText }}</span>
+      </div>
+      <div class="g-form-line">
+        <div class="g-form-btn">
+          <a class="button" @click="onLogin">注册</a>
+        </div>
+      </div>
+      <p class="g-form-error">{{ errorText }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      usernameModel: '',
+      passwordModel1: '',
+      passwordModel2: '',
+      errorText: ''
+    }
+  },
+  computed: {
+    userErrors () {
+      let errorText, status
+      if (!/@/g.test(this.usernameModel)) {
+        status = false
+        errorText = '不包含@'
+      }else {
+        status = true
+        errorText = ''
+      }
+      if (!this.userFlag) {
+        errorText = ''
+        this.userFlag = true
+      }
+      return {
+        status,
+        errorText
+      }
+    },
+    passwordErrors () {
+      let errorText, status
+      if (!/^\w{1,6}$/g.test(this.passwordModel1)) {
+        status = false
+        errorText = '密码不是1-6位'
+      }else {
+        status = true
+        errorText = ''
+      }
+      if (!this.passwordFlag) {
+        errorText = ''
+        this.passwordFlag = true
+      }
+      return {
+        status,
+        errorText
+      }
+    },
+    passwordSameErrors(){
+      let errorText,status
+      if(this.passwordModel1 !== this.passwordModel2){
+        status = false
+        errorText = '密码不一致'
+      }else{
+        status = true
+        errorText = ''
+      }
+      if(!this.passwordSameFlag){
+        errorText = ''
+        this.passwordSameFlag = true
+      }
+      return{
+        status,
+        errorText
+      }
+    }
+  },
+  methods: {
+    onLogin () {
+      if (!this.userErrors.status || !this.passwordErrors.status) {
+        this.errorText = '部分选项未通过'
+      }else {
+        this.errorText = ''
+        this.$http.get('api/login')
+        .then((res) => {
+          this.$emit('has-log', res.data)
+        }, (error) => {
+          console.log(error)
+        })
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
